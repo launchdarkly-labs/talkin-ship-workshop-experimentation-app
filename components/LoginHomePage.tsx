cda71e9ecaa9bed31a6cccb577ae810bc1e1d703
Tony <tony.w.trinh3@gmail.com>
2024-03-18 16:00:40 -0400

    deleted bank and airline components

--- components/LoginHomePage.tsx
+++ components/LoginHomePage.tsx
@@ -2,12 +2,9 @@ import { useContext } from "react";
 import { motion } from "framer-motion";
 import NavBar from "@/components/ui/navbar";
 import LoginContext from "@/utils/contexts/login";
-import BankInfoCard from "@/components/ui/bankcomponents/bankInfoCard";
+
 import { LoginComponent } from "@/components/ui/logincomponent";
-import AirlineInfoCard from "@/components/ui/airwayscomponents/airlineInfoCard";
-import airplaneImg from "@/assets/img/airways/airplane.jpg";
-import hotAirBalloonImg from "@/assets/img/airways/hotairBalloon.jpg";
-import airplaneDining from "@/assets/img/airways/airplaneDining.jpg";
+
 import MarketInfoCard from "@/components/ui/marketcomponents/marketInfoCard";
 
 interface LoginHomePageProps {
@@ -88,75 +85,9 @@ export default function LoginHomePage({ variant, name, ...props }: LoginHomePage
         </div>
       </header>
 
-      {variant === 'bank' && (
-        <section
-          className="w-3/4 grid grid-cols-2 sm:flex sm:flex-row font-sohnelight text-center justify-center mx-auto gap-y-8 
-            sm:gap-y-0 gap-x-8
-          sm:gap-x-12 lg:gap-x-24"
-        >
+    
 
-          {bankingServicesArr.map((ele,i) => {
-            return (
-              <div className="grid items-center justify-items-center" key={i}>
-                <img src={ele?.imgSrc} width={96} className="pb-2" />
-                {/* <Banknote size={96} strokeWidth={1} className="pb-2" /> */}
-                <p className="text-xl lg:text-2xl ">{ele?.title} </p>
-              </div>
-            );
-          })}
-        </section>
-      )}
-      {variant === 'bank' && (
-        <section
-          className="flex flex-col gap-y-8 sm:gap-y-8 sm:flex-row sm:gap-x-6 lg:gap-x-14
-           mx-auto py-12 justify-center px-4 lg:px-8"
-        >
-          <BankInfoCard
-            imgSrc="House.png"
-            headerTitleText="Home Mortgages"
-            subtitleText="Toggle the light on and come home. Were here to help."
-            key={1}
-          />
-          <BankInfoCard
-            imgSrc="Smoochy.png"
-            headerTitleText="Wealth Management"
-            subtitleText="Use next generation tooling to ensure your future is safe."
-            key={2}
-          />
-          <BankInfoCard
-            imgSrc="Cards.png"
-            headerTitleText="Sign Up For Toggle Card"
-            subtitleText="Special offers for our most qualified members. Terms apply."
-            key={3}
-          />
-        </section>
-      )}
-
-      {variant === 'airlines' && (
-        <section
-          className="flex flex-col gap-y-8 sm:gap-y-8 sm:flex-row sm:gap-x-6 lg:gap-x-14
-           mx-auto py-12 justify-center px-4 lg:px-8"
-        >
-
-          <AirlineInfoCard
-            headerTitleText="Wheels up"
-            subtitleText="You deserve to arrive refreshed, stretch out in one of our luxurious cabins."
-            imgSrc={airplaneImg}
-          />
-          <AirlineInfoCard
-            headerTitleText="Ready for an adventure"
-            subtitleText="The world is open for travel. Plan your next adventure."
-            imgSrc={hotAirBalloonImg}
-          />
-          <AirlineInfoCard
-            headerTitleText="Experience luxury"
-            subtitleText="Choose Launch Platinum. Select on longer flights."
-            imgSrc={airplaneDining}
-          />
-
-        </section>
-      )}
-
+  
       {variant === 'market' && (
         <section
         className="relative flex flex-col sm:flex-row justify-center 
